60ad62bb07b8aa10a3187b34f97caf4c1cbd02a0
Jose Soto <jsoto7087@gmail.com>
2023-06-11 19:57:58 -0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 puts "🌱🌱🌱🪴🪴🪴 Seeding spices..."
 
@@ -51,14 +44,5 @@ end
     meeting_id: meeting.id
   )
 end 
-
-
-# t.integer "meeting_id"
-# t.string "criteria1"
-# t.string "note1"
-# t.string "criteria2"
-# t.string "note2"
-# t.string "criteria3"
-# t.string "note3"
 
 puts "🌳🌳🌳✅ Done seeding~!"
