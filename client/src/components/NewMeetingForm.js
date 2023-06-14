652e32f35cc0178f8eda0295984812f777b8bd9d
Jose Soto <jsoto7087@gmail.com>
2023-06-14 18:40:51 -0400

    break

--- client/src/components/NewMeetingForm.js
+++ client/src/components/NewMeetingForm.js
@@ -5,11 +5,28 @@ function NewMeetingForm() {
 
   const [formData, setFormData] = useState([])
 
+
+  // t.integer "user_id"
+  // t.integer "employee_id"
+  // t.string "title"
+  // t.boolean "completed"
+  // t.datetime "scheduled_date"
+
   async function handleSubmit(e) {
 
     e.preventDefault();
 
-    
+    const response = await fetch(`/meetings`, {
+      method: "POST", 
+      headers: {'Content-Type': 'application/json'},
+      body:JSON.stringify(formData)
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+      console.log(data)
+    }
+
   }
 
   return (
