8ef4b59f1061a386a896664dc8cad39de90d325e
Jose Soto <jsoto7087@gmail.com>
2023-06-24 11:52:45 -0400

    routes work

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import '../App.css';
+import { Route, Switch } from "react-router-dom"
 import NavBar from './NavBar';
 import Auth from './Auth';
+import Meetings from './Meetings'
+import EmployeeMenu from './EmployeeMenu'
 
 function App() {
   
+  const userLogedIn = window.localStorage.getItem("isLoggedIn")
+
+  if (userLogedIn === null) return <Auth />
   
   return (
     <div>
       <NavBar />
-      <Auth />
+      <Switch>
+        <Route exact path="/">
+           <Meetings />
+        </Route>
+        <Route exact path="/supervisions">
+          <EmployeeMenu />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <UserContextProvider>
       <DataContextProvider>
         <Router>
@@ -17,7 +16,6 @@ root.render(
         </Router>
       </DataContextProvider>
     </UserContextProvider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
