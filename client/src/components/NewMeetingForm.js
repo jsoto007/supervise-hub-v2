38e26bb222d4eef5c84a604684a427f866bdc2eb
Jose Soto <jsoto7087@gmail.com>
2023-07-01 23:51:33 -0400

    error handling for new meeting

--- client/src/components/NewMeetingForm.js
+++ client/src/components/NewMeetingForm.js
@@ -15,7 +15,7 @@ function NewMeetingForm() {
 
   const [dateTime, setDateTime] = useState([]);
 
-  const [respErrors, setRespErrors] = useState([])
+  const [errors, setErrors] = useState([])
 
   const history = useHistory();
 
@@ -43,7 +43,7 @@ function NewMeetingForm() {
       handleAddMeeting(newMeeting)
       history.push("/")
     }else {
-      console.log("resp", newMeeting.error)
+      setErrors(data.error)
     }
 
   }
@@ -90,6 +90,13 @@ function NewMeetingForm() {
           class="shadow-slate-600 text-white bg-gradient-to-r from-slate-900 to-slate-700
           hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg dark:shadow-lg font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-8 mt-2"
         >Create Meeting</button>
+         {errors.length < 1 ? 
+          null
+          : 
+          <ul class="bg-red-400 mx-2 my-1 rounded-lg p-2" key={errors.login}>
+          <li class="mx-5">ⓧ  {errors.login}</li>
+          </ul>
+        }
       </form>
     </div>
   )
